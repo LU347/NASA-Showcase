067c4d8219541ae161126c208257b328b84630cf
LU347 <103535661+LU347@users.noreply.github.com>
2024-07-25 11:27:58 -0500

    Update CardContainer.jsx

--- nasa-showcase/src/CardContainer.jsx
+++ nasa-showcase/src/CardContainer.jsx
@@ -5,7 +5,6 @@ const CardContainer = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        console.log('API Key:', import.meta.env.VITE_API_KEY);
         fetch(`https://api.nasa.gov/planetary/apod?api_key=${import.meta.env.VITE_API_KEY}`)
             .then(response => response.json())
             .then(data => setData(data))
