f4ce92eb3b6ba51ca7a1a302a96d2a832732597d
LU347 <103535661+LU347@users.noreply.github.com>
2024-08-10 22:31:28 -0500

    Added Conditional Rendering

--- nasa-showcase/src/Card.jsx
+++ nasa-showcase/src/Card.jsx
@@ -4,13 +4,25 @@ const Card = ({ item }) => {
     return (
         <div className="home-grid">
             <div className="container">
-                <img 
-                    src={item.url} 
-                    alt="Placeholder" 
-                    height="auto" 
-                    width="400px" 
-                    style={{ borderRadius: '10px' }} 
-                />
+                {item.media_type === 'video' ? (
+                    <video
+                        src={item.url}
+                        controls
+                        height="auto"
+                        width="400px"
+                        style={{ borderRadius: '10px' }}
+                    >
+                        Your browser does not support the video tag.
+                    </video>
+                ) : (
+                    <img 
+                        src={item.url} 
+                        alt="APOD Image" 
+                        height="auto" 
+                        width="400px" 
+                        style={{ borderRadius: '10px' }} 
+                    />
+                )}
             </div>
             <div className="card-grid">
                 <h1>{item.title}</h1>
